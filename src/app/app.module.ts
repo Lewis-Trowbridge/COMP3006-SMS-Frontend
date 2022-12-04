ca16c878ec7acca09e8603cefca751849deeee2e
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-04 11:01:07 +0000

    Added barcode reader component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarcodeReaderComponent } from './barcode-reader/barcode-reader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarcodeReaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/barcode-reader/barcode-reader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barcode-reader',
+  templateUrl: './barcode-reader.component.html',
+  styleUrls: ['./barcode-reader.component.css']
+})
+export class BarcodeReaderComponent {
+
+}
