96e7ece6c1b22ebaac781ee59c5da5cc61e29c87
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-12 23:15:06 +0000

    Added test for rendering found cameras

--- src/app/barcode-reader/barcode-reader.component.spec.ts
+++ src/app/barcode-reader/barcode-reader.component.spec.ts
@@ -1,6 +1,11 @@
 import { BarcodeReaderComponent } from './barcode-reader.component';
 import {render} from "@testing-library/angular";
-import {ZXingScannerModule} from "@zxing/ngx-scanner";
+import {ZXingScannerComponent, ZXingScannerModule} from "@zxing/ngx-scanner";
+import {MockBuilder, MockInstance} from "ng-mocks";
+import {EventEmitter} from "@angular/core";
+import {mock} from "jest-mock-extended";
+import {AppModule} from "../app.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 describe('BarcodeReaderComponent', () => {
 
@@ -8,4 +13,27 @@ describe('BarcodeReaderComponent', () => {
     const { queryByText } = await render(BarcodeReaderComponent, { imports: [ZXingScannerModule] })
     expect(queryByText("barcode-reader works!")).toBeInTheDocument()
   });
+
+  it('should display all available video inputs', async () => {
+    const testEmitter = new EventEmitter<MediaDeviceInfo[]>()
+    const mockDevice1Name = 'mockDevice1'
+    const mockDevice1 = mock<MediaDeviceInfo>({
+      label: mockDevice1Name
+    })
+
+    const expected = [
+      mockDevice1,
+    ]
+    const moduleMetadata = MockBuilder(BarcodeReaderComponent, AppModule)
+      .mock(ZXingScannerModule)
+      .keep(ReactiveFormsModule)
+      .build()
+    MockInstance(ZXingScannerComponent, 'camerasFound', testEmitter)
+
+    const { findByText } = await render(BarcodeReaderComponent, moduleMetadata)
+
+    testEmitter.emit(expected)
+
+    expect(await findByText(mockDevice1Name)).toBeInTheDocument()
+  })
 });
