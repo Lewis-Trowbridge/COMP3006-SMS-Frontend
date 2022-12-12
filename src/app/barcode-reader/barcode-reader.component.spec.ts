bbd1ee19dc8b46f7a9d5c9c54a6bd0a2d7a48d50
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-12 17:52:04 +0000

    Converted barcode tests to fully use testing library

--- src/app/barcode-reader/barcode-reader.component.spec.ts
+++ src/app/barcode-reader/barcode-reader.component.spec.ts
@@ -1,23 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BarcodeReaderComponent } from './barcode-reader.component';
+import {render} from "@testing-library/angular";
+import {ZXingScannerModule} from "@zxing/ngx-scanner";
 
 describe('BarcodeReaderComponent', () => {
-  let component: BarcodeReaderComponent;
-  let fixture: ComponentFixture<BarcodeReaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BarcodeReaderComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(BarcodeReaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    const { queryByText } = await render(BarcodeReaderComponent, { imports: [ZXingScannerModule] })
+    expect(queryByText("barcode-reader works!")).toBeInTheDocument()
   });
 });
