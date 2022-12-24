76d23cde67f580880fbf06c89fc6f86a89db84d4
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-24 17:21:30 +0000

    Extended validation tests to stock

--- src/app/item-form/item-form.component.spec.ts
+++ src/app/item-form/item-form.component.spec.ts
@@ -7,9 +7,15 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { MatGridListModule } from '@angular/material/grid-list'
+import userEvent from '@testing-library/user-event'
 
 describe('ItemFormComponent', () => {
-  it.each(['Name', 'Barcode', 'Position'])('%s input should show validation error if value left blank', async (name) => {
+  const user = userEvent.setup()
+
+  it.each([{ name: 'Name', role: 'textbox' },
+    { name: 'Barcode', role: 'textbox' },
+    { name: 'Position', role: 'textbox' },
+    { name: 'Stock', role: 'spinbutton' }])('$name input should show validation error if value left blank', async ({ name, role }) => {
     const moduleMetadata = MockBuilder(ItemFormComponent, AppModule)
       .keep(ReactiveFormsModule)
       .keep(MatFormFieldModule)
@@ -20,9 +26,10 @@ describe('ItemFormComponent', () => {
       .build()
 
     const { findByRole, findByText } = await render(ItemFormComponent, moduleMetadata)
-    const input = await findByRole('textbox', { name })
-    input.focus()
-    input.blur()
+    const input = await findByRole(role, { name }) as HTMLInputElement
+    await user.click(input)
+    await user.keyboard('[Backspace]')
+    await user.tab()
     const errorMessage = `${name} is required.`
     const errorElement = await findByText(errorMessage)
     expect(errorElement).toHaveTextContent(errorMessage)
