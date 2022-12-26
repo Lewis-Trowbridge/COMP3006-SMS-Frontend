cb38a7f27d6194484fe449c335e37f0ad93e31e2
Lewis Trowbridge <lewbob9927@gmail.com>
2022-12-26 12:24:01 +0000

    Enabled barcode support over default QR code

--- src/app/barcode-reader/barcode-reader.component.ts
+++ src/app/barcode-reader/barcode-reader.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { FormControl } from '@angular/forms'
 import { MatDialogRef } from '@angular/material/dialog'
+import { BarcodeFormat } from '@zxing/browser'
 
 @Component({
   selector: 'app-barcode-reader',
@@ -11,6 +12,7 @@ export class BarcodeReaderComponent {
   mediaDevices: MediaDeviceInfo[] = []
   currentMediaDevice: MediaDeviceInfo | undefined
   cameraFormControl = new FormControl()
+  barcodeFormats = [BarcodeFormat.EAN_13, BarcodeFormat.CODE_128]
 
   constructor (public dialogRef: MatDialogRef<BarcodeReaderComponent>) {
     this.cameraFormControl.valueChanges.subscribe(value => {
