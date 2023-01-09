497e78c9ec75d502e05998e98c623363e2e2a6bf
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-01-09 11:49:47 +0000

    Added WIP obtaining of data on load

--- src/app/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { IShoppingListItem, ShoppingListServiceService } from '../shopping-list-socket.service'
+import { IShoppingListItem, ShoppingListServiceService as ShoppingListSocketService } from '../shopping-list-socket.service'
 import { FormArray, FormControl, FormGroup } from '@angular/forms'
 import { Observable } from 'rxjs'
+import { ShoppingListRESTService } from '../shopping-list-rest.service'
 
 interface IShoppingListItemFormGroup {
   _id: FormControl<string>
@@ -33,17 +34,31 @@ export class ShoppingListEditorComponent implements OnInit {
   }
 
   constructor (private readonly routeInfo: ActivatedRoute,
-    private readonly shoppingListSocketService: ShoppingListServiceService) { }
+    private readonly shoppingListRestService: ShoppingListRESTService,
+    private readonly shoppingListSocketService: ShoppingListSocketService) { }
 
   ngOnInit (): void {
     this.routeInfo.paramMap.subscribe(params => {
       this.listId = params.get('listId') ?? ''
-    })
+      console.log(this.listId)
+      this.shoppingListRestService.get(this.listId).subscribe(data => {
+        while (this.items.length !== 0) {
+          this.items.removeAt(0, { emitEvent: false })
+        }
+        for (const item of data.items) {
+          this.items.push(new FormGroup<IShoppingListItemFormGroup>({
+            _id: new FormControl<string>(item._id, { nonNullable: true }),
+            text: new FormControl<string>(item.text, { nonNullable: true }),
+            quantity: new FormControl<number>(item.quantity, { nonNullable: true })
+          }), { emitEvent: false })
+        }
+      })
 
-    this.shoppingListSocketService.registerChangeObservers(
-      this.listId,
-      this.items.valueChanges as Observable<IShoppingListItem[]>)
-      .subscribe(canonical => this.items.setValue(canonical))
+      this.shoppingListSocketService.registerChangeObservers(
+        this.listId,
+        this.items.valueChanges as Observable<IShoppingListItem[]>)
+        .subscribe(canonical => this.items.setValue(canonical))
+    })
   }
 
   addItem (): void {
