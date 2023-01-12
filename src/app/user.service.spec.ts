0826b9434f57338129b578e2c7c30f9126542edf
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-01-12 21:59:25 +0000

    Added tests for UserService login

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,16 +1,63 @@
+import { HttpClient } from '@angular/common/http'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { environment } from '../environments/environment'
 
 import { UserService } from './user.service'
 
 describe('UserService', () => {
   let service: UserService
 
+  let httpTestingController: HttpTestingController
+
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] })
     service = TestBed.inject(UserService)
+    TestBed.inject(HttpClient)
+    httpTestingController = TestBed.inject(HttpTestingController)
   })
 
-  it('should be created', () => {
-    expect(service).toBeTruthy()
+  describe('login', () => {
+    it('returns true when request is successful', (done) => {
+      const testUsername = 'username'
+      const testPassword = 'password'
+      const response = service.login(testUsername, testPassword)
+      response.subscribe(data => {
+        expect(data).toBe(true)
+        done()
+      })
+
+      const req = httpTestingController.expectOne({
+        url: `${environment.BACKEND_URL}/users/login`,
+        method: 'POST'
+      })
+
+      req.flush(null)
+
+      expect(req.request.body).toEqual({ username: testUsername, password: testPassword })
+
+      httpTestingController.verify()
+    })
+
+    it('returns false when request returns 401', (done) => {
+      const testUsername = 'username'
+      const testPassword = 'password'
+      const response = service.login(testUsername, testPassword)
+      response.subscribe(data => {
+        expect(data).toBe(false)
+        done()
+      })
+
+      const req = httpTestingController.expectOne({
+        url: `${environment.BACKEND_URL}/users/login`,
+        method: 'POST'
+      })
+
+      req.flush(null, { status: 401, statusText: 'Unauthorised.' })
+
+      expect(req.request.body).toEqual({ username: testUsername, password: testPassword })
+
+      httpTestingController.verify()
+    })
   })
 })
